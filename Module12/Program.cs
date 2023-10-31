40a25cc7ab1e59aa3272c4f2159cc3cdd6977067
Anton <anton.batalov94@gmail.com>
2023-10-31 20:17:05 +0700

    Добавьте файлы проекта.

---
+++ Module12/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Module12
+{
+    internal class Program
+    {
+        //task 12.1.2
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+            string greetings = $"Привет, {name}";
+            Console.WriteLine(greetings);
+        }
+    }
+}

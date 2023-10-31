5d93ddb7c4009a334d8b65f76a79a9a91f8fcee5
Anton <anton.batalov94@gmail.com>
2023-10-31 21:02:06 +0700

    task 12.1.5 done

--- Module12/Program.cs
+++ Module12/Program.cs
@@ -2,19 +2,41 @@
 {
     internal class Program
     {
-        //task 12.1.4
+        //task 12.1.5
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите свой возраст");
-            int age = int.Parse(Console.ReadLine());
-            if (age > 13)
+            List<User> users = new List<User>()
             {
-                Console.WriteLine("Вы успешно зарегестрированы");
-            }
-            else
+                new User("User1", "User1Name", false),
+                new User("User2", "User2Name", true),
+                new User("User3", "User3Name", false),
+                new User("User4", "User4Name", true),
+                new User("User5", "User5Name", true)
+            };
+
+            foreach (var user in users)
             {
-                Console.WriteLine("Пользователи младше 14 лет не могут быть зарегестрированы");
+                Console.WriteLine($"\nПриветствую, {user.Name}!");
+                if (!user.IsPremium) ShowAds();
             }
+
+
+
+        }
+
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
         }
     }
 }
